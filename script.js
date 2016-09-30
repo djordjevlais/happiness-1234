507ebb81425fbd59f03077f2a62775765a95f681
Djordje Vlaisavljevic <dzvlais@gmail.com>
2016-09-30 15:50:06 +0200

    Add initial files via upload

---
+++ script.js
@@ -0,0 +1,155 @@
+var quotes = {
+  "0": "Doing what you like is freedom. Liking what you do is happiness.\n",
+  "1": "If you look to others for fulfillment, you will never be fulfilled.\n",
+  "2": "When you realize there is nothing lacking, the world belongs to you.\n",
+  "3": "Everything is perfect either for our growth or our enjoyment.\n",
+  "4": "There is no such thing as a problem without a gift for you in its hands. You seek\n",
+  "5": "problems because you need their gifts.\n",
+  "6": "Tension is who you think you should be, relaxation is who you are.\n",
+  "7": "The art of living lies less in eliminating our troubles than growing with them.\n",
+  "8": "If you start to think the problem is \u2018out there,\u2019 stop yourself. That thought is the\n",
+  "9": "problem.\n",
+  "10": "Focus on the journey, not the destination.\n",
+  "11": "Joy is found not in finishing an activity, but in doing it.\n",
+  "12": "Happiness never decreases by being shared.\n",
+  "13": "In our lives, change is unavoidable, loss is unavoidable. In the adaptability and\n",
+  "14": "ease with which we experience change, lies our happiness and freedom.\n",
+  "15": "Blessed are those who can give without remembering and take without\n",
+  "16": "forgetting.\n",
+  "17": "He who lives in harmony with himself lives in harmony with the universe.\n",
+  "18": "All the world\u2019s a stage, and the men and women merely players. They have their\n",
+  "19": "exits and their entrances; and one man in his time plays many parts.\n",
+  "20": "When one door of happiness closes, another opens.\n",
+  "21": "Happiness is not having what you want. It is appreciating what you have.\n",
+  "22": "Most of us are just about as happy as we make up our minds to be.\n",
+  "23": "Success is getting what you want. Happiness is wanting what you get.\n",
+  "24": "Money doesn\u2019t bring happiness and creativity.Your creativity and happiness\n",
+  "25": "brings money.\n",
+  "26": "There is no stress in the world, only people thinking stressful thoughts and then\n",
+  "27": "acting on them.\n",
+  "28": "A smile is a curve that sets everything straight.\n",
+  "29": "There is only one thing more painful than learning from experience and that is\n",
+  "30": "not learning from experience.\n",
+  "31": "Anxiety is the dizziness of freedom.\n",
+  "32": "Life isn\u2019t measured by the breaths you take, but by the moments that take your\n",
+  "33": "breath away.\n",
+  "34": "Happiness is where we find it, but very rarely where we seek it.\n",
+  "35": "To be content means that you realize you contain what you seek.\n",
+  "36": "That which does not kill you makes you stronger.\n",
+  "37": "Look at everything as though you were seeing it either for the first or last time.\n",
+  "38": "Then your time on earth will be filled with glory.\n",
+  "39": "You are responsible for your life. You can\u2019t keep blaming somebody else for your\n",
+  "40": "dysfunction. Life is really about moving on.\n",
+  "41": "If you want to enjoy the rainbow, be prepared to endure the storm.\n",
+  "42": "Expecting life to treat you well because you are a good person is like expecting\n",
+  "43": "an angry bull not to charge because you are a vegetarian.\n",
+  "44": "Yesterday is the past.Tomorrow is the future. Today is a gift and that\u2019s why we\n",
+  "45": "call it the present.\n",
+  "46": "He who dares nothing need hope for nothing.\n",
+  "47": "A coward gets scared and quits. A hero gets scared, but still goes on.\n",
+  "48": "When we cannot find contentment in ourselves, it is useless to seek it elsewhere.\n",
+  "49": "Everyone wants happiness. No one wants pain. But you can\u2019t have a rainbow,\n",
+  "50": "without a little rain.\n",
+  "51": "The tough times are what make our achievements feel so exhilarating; we need\n",
+  "52": "that dynamic.\n",
+  "53": "Feel the fear and do it anyway.\n",
+  "54": "Knowing is not enough, we must apply; willing is not enough, we must do.\n",
+  "55": "There is only one cause of unhappiness: the false beliefs you have in your head,\n",
+  "56": "beliefs so widespread, so commonly held, that it never occurs to you to question\n",
+  "57": "them.\n",
+  "58": "Yesterday I was clever, so I wanted to change the world. Today I am wise, so I am changing myself.\n",
+  "59": "Start by doing what's necessary, then do what's possible; and suddenly you are\n",
+  "60": "doing the impossible.\n",
+  "61": "We are like the spider. We weave our life and then move along in it. We are like\n",
+  "62": "the dreamer who dreams and then lives in the dream.This is true for the entire\n",
+  "63": "universe.\n",
+  "64": "Always be on the lookout for ways to turn a problem into an opportunity for\n",
+  "65": "success.\n",
+  "66": "All our dream can come true, if we have the courage to pursue them.\n",
+  "67": "There is only one thing that makes a dream impossible to achieve: the fear of\n",
+  "68": "failure.\n",
+  "69": "If you can dream it, you can do it. Always remember that this whole thing was\n",
+  "70": "started with a dream and a mouse.\n",
+  "71": "I don't dream at night, I dream all day; I dream for a living.\n",
+  "72": "The biggest adventure you can ever take is to live the life of your dreams. \n",
+  "73": "There is nothing either good or bad, but thinking makes it so.\n",
+  "74": "What oxygen is to the lungs, such is hope to the meaning of life.\n",
+  "75": "Our greatest weakness lies in giving up. The most certain way to succeed is\n",
+  "76": "always to try just one more time.\n",
+  "77": "In optimism there is magic. In pessimism there is nothing.\n",
+  "78": "Don't ask yourself what the world needs. Ask yourself what makes you come\n",
+  "79": "alive and then go do that. Because what the world needs is people who have\n",
+  "80": "come alive.\n",
+  "81": "You will either step forward into growth or step backward into safety.\n",
+  "82": "Limitations live only in our minds. But if we use our imaginations, our\n",
+  "83": "possibilities become limitless.\n",
+  "84": "You will know the truth, and the truth will make you free.\n",
+  "85": "By three methods we may learn wisdom: First, by reflection, which is noblest;\n",
+  "86": "Second, by imitation, which is easiest; and third by experience, which is the\n",
+  "87": "bitterest.\n",
+  "88": "Knowing yourself is the beginning of all wisdom.\n",
+  "89": "We cannot always build the future for our youth, but we can build our youth for\n",
+  "90": "the future.\n",
+  "91": "It is your decisions not your conditions that truly shape the quality of your life.\n",
+  "92": "Optimism may sometimes be delusional, but pessimism is always delusional.\n",
+  "93": "We are continually faced with great opportunities which are brilliantly disguised\n",
+  "94": "as unsolvable problems.\n",
+  "95": "If you just set out to be liked, you would be prepared to compromise on anything\n",
+  "96": "at any time, and you would achieve nothing.\n",
+  "97": "You have within you right now, everything you need to deal with whatever the\n",
+  "98": "world can throw at you.\n",
+  "99": "The leader has to be practical and a realist, yet must talk the language of the\n",
+  "100": "visionary and the idealist.\n",
+  "101": "Anyone who has never made a mistake has never tried anything new.\n",
+  "102": "When the best leader\u2019s work is done the people say, \u2018We did it ourselves\u2019.\n",
+  "103": "Luck favors the well prepared.\n",
+  "104": "It is not because things are difficult that we do not dare, it is because we do not\n",
+  "105": "dare that things are difficult.\n",
+  "106": "Optimism is a strategy for making a better future.\n",
+  "107": "The possible ranks higher than the actual.\n",
+  "108": "Experience is not what happens to a man, it is what a man does with what\n",
+  "109": "happens to him.\n",
+  "110": "Imagination rules the world.\n"
+};
+
+
+/** Umesto da za prikaz svake quote biramo random num, shufflovacemo niz indeksa
+i preko tog niza iterirati kroz quotes
+**/
+
+
+// Durstenfeld shuffle algorithm
+ function shuffle(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
+// Create array in range [0, number of quotes]
+var array = Array.apply(null, {
+  length: Object.keys(quotes).length
+}).map(Function.call, Number);
+
+// Shuffle brt
+shuffle(array);
+
+// Initialize quotes/populate document
+var i = 0;
+
+$("#container").find(".quote").each(function(index) {
+  $(this).text(quotes[array[i]]);
+  i++;
+});
+
+// Swap quote on button click
+$(document).on("click", ".close", function() {
+  if (i < Object.keys(quotes).length) {
+    $(this).next().text(quotes[array[i]]);
+    i++;
+  }
+});
+
